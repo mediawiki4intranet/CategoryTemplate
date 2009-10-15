3bd7e286d01ea67005bae960e53e2a19622e58bd
Vitaliy Filippov <vitalif@yourcmc.ru>
2009-10-15 10:47:51 +0000

    Bug 55089 Handle Image:xxxx

--- CategoryTemplate.php
+++ CategoryTemplate.php
@@ -27,34 +27,25 @@ function wfCategoryTemplate()
 {
     global $wgHooks;
     wfLoadExtensionMessages('CategoryTemplate');
-    $wgHooks[EditFormPreloadText][] = '_cattemplateaddcategory';
     $wgHooks[CategoryPageView][] = '_cattemplatecategorychange';
-}
-
-function _cattemplateaddcategory(&$text)
-{
-    global $wgLang;
-    $cname = $_GET[_category];
-    $wnew = $_GET[_new];
-    $namespaceNames = $wgLang->getNamespaces();
-    if ($wnew == 1)
-    {
-        if (($title = Title::newFromText($namespaceNames[NS_TEMPLATE].":".$namespaceNames[NS_CATEGORY].":$cname")) &&
-            ($rev = Revision::newFromId($title->getLatestRevID())))
-            $text = $rev->getText();
-        else
-            $text = "\n\n\n[[".$namespaceNames[NS_CATEGORY].":$cname]]";
-    }
-    return true;
 }
 
 function _cattemplatecategorychange($catpage)
 {
-    global $wgOut, $wgScript, $CategoryTemplateMessages;
+    global $wgOut, $wgScript, $wgContLang, $wgCanonicalNamespaceNames, $CategoryTemplateMessages;
     $boxtext = addslashes(wfMsg("addcategorytemplate-create-article"));
     $btext = addslashes(wfMsg("addcategorytemplate-submit"));
     $confirmtext = addslashes(wfMsg("addcategorytemplate-confirm"));
     $Action = htmlspecialchars($wgScript);
+    $s1 = addslashes($wgCanonicalNamespaceNames[NS_IMAGE]);
+    $s2 = addslashes($wgContLang->getNsText(NS_IMAGE));
+    $cat = $catpage->mTitle->getText();
+    if (($title = Title::newFromText($wgContLang->getNsText(NS_TEMPLATE).":".$wgContLang->getNsText(NS_CATEGORY).":".$cat)) &&
+        ($rev = Revision::newFromId($title->getLatestRevID())))
+        $text = $rev->getText();
+    else
+        $text = "\n\n\n[[".$wgContLang->getNsText(NS_CATEGORY).":$cat]]";
+    $text = str_replace("\n", "\\n", addslashes($text));
     $temp2 = <<<ENDFORM
 <!-- Add Article Extension Start -->
 <script type="text/javascript">
@@ -63,18 +54,37 @@ function addText(thefield) { if (thefield.value=="") thefield.value=thefield.def
 function checkname()
 {
     var inp = document.getElementById('createboxInput');
-    //if (inp.value
+    var l = 0;
+    if (inp.value.substr(0, l = "$s1:".length) != "$s1:")
+    {
+        l = 0;
+        if (inp.value.substr(0, l = "$s2:".length) != "$s2:")
+            l = 0;
+    }
+    if (l)
+    {
+        document.createbox.wpDestFile.value = inp.value.substr(l);
+        document.createbox.wpUploadDescription.value = "$text";
+        document.createbox.wpTextbox1.value = "";
+        inp.value = 'Special:Upload';
+    }
+    else
+    {
+        document.createbox.wpUploadDescription.value = "";
+        document.createbox.wpTextbox1.value = "$text";
+    }
     return inp.value!=inp.defaultValue || confirm("$confirmtext".replace("%s", document.createbox.createboxInput.value));
 }
 </script>
 <table border="0" align="right" width="423" cellspacing="0" cellpadding="0">
 <tr><td width="100%" align="right" bgcolor="">
-<form name="createbox" action="{$Action}" method="get" class="createbox" onsubmit="return checkname()">
+<form name="createbox" action="{$Action}" method="POST" class="createbox" onsubmit="return checkname()">
     <input type="hidden" name="action" value="edit">
-    <input type="hidden" name="_new" value="1">
-    <input type="hidden" name="_category" value="{$catpage->mTitle->getText()}">
-    <input id="createboxInput" class="createboxInput" name="title" type="text" value="{$boxtext}" size="30" style="color:#666;" onfocus="clearText(this);" onblur="addText(this);" />
-    <input type="submit" name="create" class="createboxButton" value="{$btext}" />
+    <input type="hidden" name="wpDestFile" value="">
+    <input type="hidden" name="wpUploadDescription" value="">
+    <input type="hidden" name="wpTextbox1" value="">
+    <input id="createboxInput" class="createboxInput" name="title" type="text" value="$boxtext" size="30" style="color:#666;" onfocus="clearText(this);" onblur="addText(this);" />
+    <input type="submit" name="create" class="createboxButton" value="$btext" />
 </form>
 </td></tr>
 </table>
